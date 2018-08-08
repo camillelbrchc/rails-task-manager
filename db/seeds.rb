f06d91fd6b31770ed9880acbd8b8fa351decfa16
Camille Le Bruchec <Camille.lebruchec@gmail.com>
2018-08-08 08:45:16 +0200

    task manager

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Task.create({title: 'laundry', details: 'faire la lessive', completed: false})
+Task.create({title: 'dishes', details: 'faire la vaisselle', completed: false})
